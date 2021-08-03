a98d02419f64683b4b9f06416e230e69b40d7bca
rjwen2045458 <rjwen2045458@163.com>
2021-08-03 15:43:02 +0100

    AB reaction simulation demo

---
+++ KappaNEURON/ABSpatialKappaSimulation.py
@@ -0,0 +1,217 @@
+import SpatialKappa
+import matplotlib.pyplot as plt
+
+flog = open("/tmp/SpatialKappa-test.log", "w")
+sk = SpatialKappa.SpatialKappa(redirect_stdout=flog)
+sim = sk.kappa_sim("ms", True)
+
+sim.loadFile("ab.ka")
+
+time_record = []
+A_record = []
+B_record = []
+AB_record = []
+
+for i in range(1000):
+    sim.runForTime(0.1, True)
+    time_record.append(sim.getTime())
+    obs_A = sim.getObservation("A")
+    A_record.append(obs_A)
+    obs_B = sim.getObservation("B")
+    B_record.append(obs_B)
+    AB_record.append(sim.getObservation("AB"))
+
+plt.plot(time_record,A_record,label = "A")
+plt.plot(time_record,B_record,label = "B")
+plt.plot(time_record,AB_record,label = "AB")
+plt.xlabel("Time(ms)")
+plt.ylabel("Number of molecule")
+
+plt.legend()
+plt.show()
+
+
+
+    # def test_setSeed(self):
+    #     flog1 = open("/tmp/SpatialKappa-seed-1.log", "w")
+    #     sk = SpatialKappa.SpatialKappa(redirect_stdout=flog1)
+    #     sim1 = sk.kappa_sim("ms", True, 1)
+    #     sim1.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/caPump.ka")
+    #     sim1.runForTime(5.0, True)
+    #
+    #     flog2 = open("/tmp/SpatialKappa-seed-2.log", "w")
+    #     sk2 = SpatialKappa.SpatialKappa(redirect_stdout=flog2)
+    #     sim2 = sk2.kappa_sim("ms", True, 1)
+    #     sim2.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/caPump.ka")
+    #     sim2.runForTime(5.0, True)
+    #
+    #     self.assertEqual(sim1.getVariable('ca'), sim2.getVariable('ca'))
+    #     self.assertEqual(sim1.getVariable('P-Ca'), sim2.getVariable('P-Ca'))
+    #     self.assertEqual(sim1.getVariable('P'), sim2.getVariable('P'))
+    #
+    #     sim1 = []
+    #     sk1 = []
+    #     sim2 = []
+    #     sk2 = []
+    #
+    #     flog3 = open("/tmp/SpatialKappa-seed-3.log", "w")
+    #     sk = SpatialKappa.SpatialKappa(redirect_stdout=flog3)
+    #     sim = sk.kappa_sim("ms", True)
+    #     sim.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/caPump.ka")
+    #     sim.runForTime(5.0, True)
+    #     sim = []
+    #     sk = []
+    #
+    #     ## flog1.close()
+    #     ## flog2.close()
+    #     ## flog3.close()
+    #
+    # def test_createSpatialKappa(self):
+    #     self.sk = SpatialKappa.SpatialKappa()
+    #
+    # def test_runForTime(self):
+    #     self.sim.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/caPump.ka")
+    #     self.sim.runForTime(100.0, False)
+    #     self.assertEqual(self.sim.getTime(), 100.0)
+    #
+    # def test_addTransition(self):
+    #     self.sim.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/caPump.ka")
+    #     self.sim.addTransition("TEST", {}, {"ca": {"x": {}}}, 0.0)
+    #     self.sim.initialiseSim()
+    #     tran = self.sim.getTransition("TEST")
+    #     self.assertEqual(str(tran), "'TEST' : [] -> [[ca(x)]] @ 0.0")
+    #     self.sim.setTransitionRateOrVariable("TEST", 100.0)
+    #     self.assertEqual(str(tran), "'TEST' : [] -> [[ca(x)]] @ 100.0")
+    #     self.sim.addTransition("TEST2", {}, {"ca": {}}, 66.0)
+    #     self.sim.initialiseSim()
+    #     tran2 = self.sim.getTransition("TEST2")
+    #     self.assertEqual(str(tran2), "'TEST2' : [] -> [[ca()]] @ 66.0")
+    #     self.sim.addTransition("TEST3", {"P": {}}, {}, 77.0)
+    #     self.sim.initialiseSim()
+    #     tran3 = self.sim.getTransition("TEST3")
+    #     self.assertEqual(str(tran3), "'TEST3' : [[P()]] -> [] @ 77.0")
+    #
+    #
+    # def test_addAgent(self):
+    #     self.sim.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/caPump.ka")
+    #     self.sim.initialiseSim()
+    #     obs = self.sim.getObservation("ca")
+    #     self.assertTrue(isinstance(obs, float))
+    #     ## Add an integer number of Ca ions specified as a float
+    #     self.sim.addAgent('ca', 11.0)
+    #     obs2 = self.sim.getObservation("ca")
+    #     self.assertEqual(obs2, obs + 11.0)
+    #     ## Add an integer number of Ca ions specified as an integer
+    #     self.sim.addAgent('ca', 10)
+    #     obs3 = self.sim.getObservation("ca")
+    #     self.assertEqual(obs3, obs2 + 10)
+    #
+    #     ## Somthing like this should work, but it doesn't.
+    #     ## self.assertRaises(TypeError, random.shuffle, (1,2,3))
+    #     ## Hence the hack below
+    #     error = False
+    #     try:
+    #         self.sim.addAgent("ca", 11.5)
+    #     except Py4JJavaError:
+    #         error = True
+    #     self.assertTrue(error)
+    #
+    # def test_addVariable(self):
+    #     self.sim.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/caPump.ka")
+    #     ## 1. Add agent expression
+    #     ## Create an error due to nonexistent site name
+    #     error = False
+    #     try:
+    #         self.sim.addVariable("test", {"ca": {"nonexistent_site_name": {"l": "?"}}})
+    #     except Py4JJavaError:
+    #         error = True
+    #     self.assertTrue(error)
+    #
+    #     ## Create an error due to nonexistent site attribute
+    #     error = False
+    #     try:
+    #         self.sim.addVariable("test", {"ca": {"x": {"nonexistent_site_attribute": "?"}}})
+    #     except Py4JJavaError:
+    #         error = True
+    #     self.assertTrue(error)
+    #
+    #     self.sim.addVariable('TotCa2', {"ca": {"x": {"l": "?"}}})
+    #     self.sim.initialiseSim()
+    #     TotCa1 = self.sim.getVariable("TotCa")
+    #     TotCa2 = self.sim.getVariable("TotCa2")
+    #     self.assertEqual(TotCa1, TotCa2)
+    #     self.sim.addVariable('P-Ca2', {"ca": {"x": {"l": "1"}}, "P": {"x": {"l": "1"}}})
+    #     PCa1 = self.sim.getVariable("P-Ca")
+    #     PCa2 = self.sim.getVariable("P-Ca2")
+    #     self.assertEqual(PCa1, PCa2)
+    #
+    #     ## print PCa1, PCa2
+    #
+    #     ## 2. Add float variable
+    #     self.sim.addVariable("V", 9.0)
+    #
+    # # Test exception thrown with faulty Kappa file
+    # def test_noInit(self):
+    #     self.sk = SpatialKappa.SpatialKappa()
+    #     self.sim = self.sk.kappa_sim("ms", True)
+    #     self.sim.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/no_init.ka")
+    #     self.assertTrue(self.sim.isAgent('ca'))
+    #     self.assertFalse(self.sim.isInitialised())
+    #     self.sim.initialiseSim()
+    #     self.assertTrue(self.sim.isInitialised())
+    #     # error = False
+    #     # try:
+    #     #     self.sim.getAgentDeclaration('ca')
+    #     # except Py4JJavaError:
+    #     #     error = True
+    #     # self.assertTrue(error)
+    #
+    # def test_agentDeclaration(self):
+    #     self.sim.addAgentDeclaration('A', {'x': []})
+    #     self.sim.addAgentDeclaration('B', {'x': ['a', 'b', 'c']})
+    #     self.sim.addAgentDeclaration('C', {'x': ['a', 'b', 'c'], 'y': ['d', 'e', 'f']})
+    #
+    #     ## This doesn't work becuase of the type of the returned objects being Java maps
+    #     ## self.assertEqual({u'x': []}, {u'x': []})
+    #     ## self.assertDictContainsSubset(dict(self.sim.getAgentDeclaration('A')), {u'x': []})
+    #     self.assertEqual(list(self.sim.getAgentDeclaration('A')['x']), [])
+    #     self.assertEqual(list(self.sim.getAgentDeclaration('B')['x']), ['a', 'b', 'c'])
+    #     self.assertEqual(list(self.sim.getAgentDeclaration('C')['x']), ['a', 'b', 'c'])
+    #     self.assertEqual(list(self.sim.getAgentDeclaration('C')['y']), ['d', 'e', 'f'])
+    #     ## Expect error, since no intialisation
+    #     # try:
+    #     #     self.sim.getAgentMap('A')
+    #     # except Py4JJavaError:
+    #     #     error = True
+    #     # self.assertTrue(error)
+    #
+    # def test_getAgentDeclaration(self):
+    #     self.sim.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/caPump.ka")
+    #     self.assertEqual(list(self.sim.getAgentDeclaration("ca")['x']), [])
+    #
+    # def test_overrideInitialValue(self):
+    #     self.sim.addAgentDeclaration('A', {'x': []})
+    #     self.sim.overrideInitialValue({'A': {'x': {}}}, 1)
+    #     ## self.sim.overrideInitialValue({"ca": {"nonexistent_site_name": {"l": "?"}}}, 2)
+    #
+    # def test_getVariableComplex(self):
+    #     self.sim.loadFile(os.path.dirname(SpatialKappa.__file__) + "/tests/caPump.ka")
+    #
+    #     try:
+    #         self.sim.getVariableComplex('gamma1')
+    #     except Py4JJavaError:
+    #         error = True
+    #     self.assertTrue(error)
+    #     self.sim.getVariableComplex('P-Ca')
+    #     self.sim.agentList(self.sim.getVariableComplex('P-Ca'))
+    #     self.sim.overrideInitialValue(self.sim.agentList(self.sim.getVariableComplex('P-Ca')), 10)
+    #     self.sim.initialiseSim()
+    #     self.assertEqual(self.sim.getVariable('P-Ca'), 10)
+    #
+    # def tearDown(self):
+    #     self.sim = []
+    #     self.sk = []
+    #     self.flog.close()
+#
+# if __name__ == '__main__':
+#     unittest.main()
---
+++ KappaNEURON/ExternalSimulatorInterfaceNEURON.py
@@ -0,0 +1,113 @@
+import os
+import re
+import SpatialKappa
+from py4j.protocol import Py4JJavaError
+from neuron import h, rxd
+from neuron import nonvint_block_supervisor as nbs
+from neuron.units import mV, ms, uM
+import matplotlib.pyplot as plt
+
+h.load_file("stdrun.hoc")
+gateway = SpatialKappa.SpatialKappa(redirect_stdout=None)
+
+def report(mess):
+    global verbose
+    if (verbose):
+        print(mess)
+
+class ExternalSimulatorInterface():
+    def __init__(self, region, *args, **kwargs):
+        self._species = kwargs.get('species', [])
+        kappa_file = kwargs.get('kappa_file')
+        time_units = kwargs.get('time_units', 'ms')
+        self.seed = kwargs.get('seed', None)
+        self._sk_redirect_stdout = kwargs.get('sk_redirect_stdout', None)
+        self._time_units = time_units
+        self._kappa_file = os.path.join(os.getcwd(), kappa_file)
+        self.kappa_sim = None
+
+        self._region = region
+        self._callbacks = [
+            None,  # setup
+            self.init,  # initialize
+            None,  # current
+            None,  # conductance
+            self.run_every_fixed_timestep,  # fixed step advance
+            None,  # ode_count
+            None,  # ode_reinit
+            None,  # rhs
+            None,  # ode_solve
+            None,  # ode_jacobian
+            None,  # ode_abs_tolerance
+        ]
+        self.register()
+
+        nodelist = self._species[0][self._region].nodes
+        self._segments = [n.segment for n in nodelist]
+
+    def init(self):
+        global gateway
+
+        self._kappa_sims = []  # Will this destroy things properly?
+        self._name = dict()
+
+        for segment in self._segments:
+            kappa_sim = gateway.kappa_sim(self._time_units, True, self.seed)
+            self._name[segment] = kappa_sim
+
+            try:
+                kappa_sim.loadFile(self._kappa_file)
+            except Py4JJavaError as e:
+                java_err = re.sub(r'java.lang.IllegalStateException: ', r'', str(e.java_exception))
+                errstr = 'Error in kappa file %s: %s' % (self._kappa_file, java_err)
+                raise RuntimeError(errstr)
+            kappa_sim.initialiseSim()
+            self._kappa_sims.append(kappa_sim)
+
+    def run_every_fixed_timestep(self, dt):
+
+        for kappa_sim in self._kappa_sims:
+            kappa_sim.runForTime(dt, False)
+
+        for s in self._species:
+            nodelist = s[self._region].nodes
+            for n in nodelist:
+                print(s.name)
+                kappa_sim = self._name[n.segment]
+                n.concentration = kappa_sim.getObservation(s.name) #/ (n.volume * 6.022 * pow(10, 5))
+
+    def register(self):
+        nbs.register(self._callbacks)
+
+    def unregister(self):
+        nbs.unregister(self._callbacks)
+
+
+dend = h.Section(name="dend")
+dend.nseg = 3
+cyt = rxd.Region([dend], name="cyt")
+A = rxd.Species([cyt], name="A", charge=0, initial=0 * uM)
+B = rxd.Species([cyt], name="B", charge=0, initial=0 * uM)
+AB = rxd.Species([cyt], name="AB", charge=0, initial=0 * uM)
+
+t = h.Vector().record(h._ref_t)
+A_cyt01 = h.Vector().record(A[cyt].nodes(dend(0.1))._ref_concentration)
+B_cyt01 = h.Vector().record(B[cyt].nodes(dend(0.1))._ref_concentration)
+AB_cyt01 = h.Vector().record(AB[cyt].nodes(dend(0.1))._ref_concentration)
+
+es1 = ExternalSimulatorInterface(cyt, species=[A, B, AB], kappa_file="ab.ka")
+
+h.finitialize(-65 * mV)
+h.continuerun(100 * ms)
+
+A_cyt01[0] = 150
+B_cyt01[0] = 100
+
+plt.plot(t, A_cyt01, label="A")
+plt.plot(t, B_cyt01, label="B")
+plt.plot(t, AB_cyt01, label="AB")
+plt.xlabel("Time(ms)")
+plt.ylabel("Number of molecule")
+
+plt.legend()
+plt.show()
